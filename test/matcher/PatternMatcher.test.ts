b766d83acaacc1e52aef020f7648e846e542939d
Joe <56809242+Jo3-L@users.noreply.github.com>
2021-08-17 22:34:33 -0700

    test(pattern-matcher): increase test coverage

--- test/matcher/PatternMatcher.test.ts
+++ test/matcher/PatternMatcher.test.ts
@@ -29,6 +29,19 @@ describe('constructor', () => {
 				}),
 		).toThrow(new Error('Unexpected empty blacklisted term.'));
 	});
+
+	it('should not accept patterns with optionals that have the empty string in their match set', () => {
+		expect(
+			() =>
+				new PatternMatcher({
+					blacklistedPatterns: [{ id: 10, pattern: pattern`[abc]` }],
+				}),
+		).toThrow(
+			new Error(
+				'Unexpected pattern that matches on the empty string; this is probably due to a pattern comprised of a single optional construct.',
+			),
+		);
+	});
 });
 
 describe('PatternMatcher#setInput()', () => {
@@ -129,6 +142,15 @@ describe('simple matching; no wildcards/optionals', () => {
 				1: [[117, 124]],
 			},
 		],
+		[
+			'should match several similar terms',
+			['thing', 'thang'],
+			'im just doin my thign thing ok thang',
+			{
+				0: [[22, 26]],
+				1: [[31, 35]],
+			},
+		],
 	])('%s', (_, pats, input, matches) => {
 		const expected: MatchPayload[] = [];
 		for (const [id, locs] of Object.entries(matches)) {
@@ -149,13 +171,90 @@ describe('simple matching; no wildcards/optionals', () => {
 });
 
 describe('matching with optionals', () => {
-	it.todo('tests');
+	it('should emit matches with the correct ID', () => {
+		const ms = new PatternMatcher({ blacklistedPatterns: [{ id: 10, pattern: pattern`w[o]rld` }] })
+			.setInput('world wrld')
+			.getAllMatches();
+		expect(ms.length).toBe(2);
+		expect(ms[0].termId).toBe(10);
+		expect(ms[1].termId).toBe(10);
+	});
+
+	it.each([
+		[
+			'should match a single pattern with an optional at the start',
+			['[a]bc'],
+			'abc bc',
+			{
+				0: [
+					[0, 2, 3],
+					[1, 2, 2],
+					[4, 5, 2],
+				],
+			},
+		],
+		[
+			'should match a single pattern with an optional at the end',
+			['bc[d]'],
+			'cant think of any good strings bc :(d',
+			{
+				0: [[31, 32, 2]],
+			},
+		],
+		[
+			'should match a single pattern with an optional in the middle',
+			['b[c]d'],
+			'getting tired of writing tests, bcd bd :P',
+			{
+				0: [
+					[32, 34, 3],
+					[36, 37, 2],
+				],
+			},
+		],
+		[
+			'should match a single pattern with an optional wildcard',
+			['pi[?]kle'],
+			'pickles are good and so are pikles, whatever those are',
+			{
+				0: [
+					[0, 5, 6],
+					[28, 32, 5],
+				],
+			},
+		],
+		[
+			'should match several patterns with optionals',
+			['s[?]m[e]th', 's[?]metimes'],
+			'sometimes i like smth',
+			{
+				0: [[17, 20, 4]],
+				1: [[0, 8, 9]],
+			},
+		],
+	])('%s', (_, pats, input, matches) => {
+		const expected: MatchPayload[] = [];
+		for (const [id, data] of Object.entries(matches)) {
+			const idNum = Number(id);
+			for (const datum of data) {
+				expected.push({
+					termId: idNum,
+					startIndex: datum[0],
+					endIndex: datum[1],
+					matchLength: datum[2],
+				});
+			}
+		}
+
+		const matcher = new PatternMatcher({ blacklistedPatterns: assignIncrementingIds(pats.map(parseRawPattern)) });
+		expectThatArrayIsPermutationOfOther(matcher.setInput(input).getAllMatches(), expected);
+	});
 });
 
 describe('matching with wildcards', () => {
 	it.each([
 		[
-			'should match a single pattern with an wildcard at the end',
+			'should match a single pattern with an wildcard at the end correctly',
 			['hello?'],
 			'hellom world',
 			{
@@ -163,7 +262,7 @@ describe('matching with wildcards', () => {
 			},
 		],
 		[
-			'should match a single pattern with a wildcard at the start',
+			'should match a single pattern with a wildcard at the start correctly',
 			['?world'],
 			'my world',
 			{
@@ -171,11 +270,53 @@ describe('matching with wildcards', () => {
 			},
 		],
 		[
-			'should match a single pattern with a wildcard in the middle',
+			'should match a single pattern with a wildcard in the middle correctly',
 			['?world?'],
 			'the world!',
 			{
 				0: [[3, 9]],
+			},
+		],
+		[
+			'should match several patterns with wildcards in varying positions correctly',
+			['?start', 'end?', 'mid?le?'],
+			'look, wildcards can be at the start, the end, or the middle!',
+			{
+				0: [[29, 34]],
+				1: [[41, 44]],
+				2: [[53, 59]],
+			},
+		],
+		[
+			'should match two patterns where the first is a proper suffix of the latter and has a wildcard correctly',
+			['hello', 'ell?'],
+			'hey, hello there!',
+			{
+				0: [[5, 9]],
+				1: [[6, 9]],
+			},
+		],
+		[
+			'should match four patterns where the first is a proper suffix of the second, similar with the second and so on',
+			['l?', 'll?', 'ell?', 'hello'],
+			'test test test hello??',
+			{
+				0: [
+					[17, 18],
+					[18, 19],
+				],
+				1: [[17, 19]],
+				2: [[16, 19]],
+				3: [[15, 19]],
+			},
+		],
+		[
+			'should treat surrogate pairs as a single character and thus match a wildcard',
+			['night', 'cool ?'],
+			'what a cool 🌉 night sky',
+			{
+				0: [[15, 19]],
+				1: [[7, 13]],
 			},
 		],
 	])('%s', (_, pats, input, matches) => {
