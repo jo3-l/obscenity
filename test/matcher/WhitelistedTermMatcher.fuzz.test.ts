e5f35fbad4b4843d7e2804156520261cd8e82b99
Joe <56809242+Jo3-L@users.noreply.github.com>
2021-08-18 21:29:52 -0700

    test(whitelist-term-matcher): check for permutation in fuzz test

--- test/matcher/WhitelistedTermMatcher.fuzz.test.ts
+++ test/matcher/WhitelistedTermMatcher.fuzz.test.ts
@@ -2,11 +2,16 @@ import fc from 'fast-check';
 import { Interval } from '../../src/matcher/interval/Interval';
 import { WhitelistedTermMatcher } from '../../src/matcher/WhitelistedTermMatcher';
 
+function expectThatArrayIsPermutationOfOther<T>(as: T[], bs: T[]) {
+	expect(as).toStrictEqual(expect.arrayContaining(bs));
+	expect(bs).toStrictEqual(expect.arrayContaining(as));
+}
+
 test('running the whitelisted term matcher with a set of terms s and input i should have the same result as naively matching the terms using indexOf', () => {
 	fc.assert(
 		fc.property(fc.array(fc.string16bits().filter((s) => s.length > 0)), fc.string16bits(), (terms, input) => {
 			const matcher = new WhitelistedTermMatcher({ terms });
-			expect([...matcher.getMatchedSpans(input)]).toStrictEqual(naiveMatch(terms, input));
+			expectThatArrayIsPermutationOfOther([...matcher.getMatchedSpans(input)], naiveMatch(terms, input));
 		}),
 	);
 });
