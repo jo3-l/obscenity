112bbeebc30cb3d95c14cce19b7e4e4d7dde2aba
Joe <56809242+Jo3-L@users.noreply.github.com>
2021-08-19 17:04:49 -0700

    chore(jest): remove superfluous display name

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,6 @@
 import type { Config } from '@jest/types';
 
 const config: Config.InitialOptions = {
-	displayName: 'unit test',
 	preset: 'ts-jest',
 	testEnvironment: 'node',
 	testRunner: 'jest-circus/runner',
