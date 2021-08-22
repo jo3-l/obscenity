bd9a373514badb63c59106f67f9ab6f535f0d38e
Joe <56809242+Jo3-L@users.noreply.github.com>
2021-08-22 13:23:32 -0700

    chore(example): add example code from readme

---
+++ example/basic.js
@@ -0,0 +1,18 @@
+const {
+	PatternMatcher,
+	englishDataset,
+	englishRecommendedBlacklistMatcherTransformers,
+	englishRecommendedWhitelistMatcherTransformers,
+} = require('../dist');
+
+// Creating a matcher is somewhat expensive, so do this only once in your app if possible:
+const matcher = new PatternMatcher({
+	...englishDataset.build(),
+	blacklistMatcherTransformers: englishRecommendedBlacklistMatcherTransformers,
+	whitelistMatcherTransformers: englishRecommendedWhitelistMatcherTransformers,
+});
+
+const input = 'ʃʃᵤс𝗄 you';
+console.log(
+	matcher.setInput(input).hasMatch() ? 'The input contains obscenities.' : 'The input does not contain obscenities.',
+);
---
+++ example/censor.js
@@ -0,0 +1,18 @@
+const {
+	TextCensor,
+	PatternMatcher,
+	englishDataset,
+	englishRecommendedBlacklistMatcherTransformers,
+	englishRecommendedWhitelistMatcherTransformers,
+} = require('../dist');
+
+const matcher = new PatternMatcher({
+	...englishDataset.build(),
+	blacklistMatcherTransformers: englishRecommendedBlacklistMatcherTransformers,
+	whitelistMatcherTransformers: englishRecommendedWhitelistMatcherTransformers,
+});
+const censor = new TextCensor();
+
+const input = 'you are a fucking retard';
+const matches = matcher.setInput(input).getAllMatches();
+console.log(censor.applyTo(input, matches));
