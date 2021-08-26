d2c70b73972521931359f2af3226fe21c897e992
Joe <56809242+Jo3-L@users.noreply.github.com>
2021-08-26 13:16:58 -0700

    docs: rename example to examples

--- example/basic.js
+++
@@ -1,9 +0,0 @@
-const { RegExpMatcher, englishDataset, englishRecommendedTransformers } = require('../dist');
-
-const matcher = new RegExpMatcher({
-	...englishDataset.build(),
-	...englishRecommendedTransformers,
-});
-
-const input = 'ʃʃᵤс𝗄 you';
-console.log(matcher.hasMatch(input) ? 'The input contains obscenities.' : 'The input does not contain obscenities.');
--- examples/repl.js
+++ examples/repl.js
@@ -1,24 +1,38 @@
+// A REPL where you can enter text and see whether Obscenity matches on it with
+// its English preset.
+
+// Import the REPL built-in package.
 const repl = require('repl');
+// Import what we need from Obscenity.
 const { RegExpMatcher, englishDataset, englishRecommendedTransformers } = require('../dist');
 
+// Create our matcher, using the English preset.
 const matcher = new RegExpMatcher({
 	...englishDataset.build(),
 	...englishRecommendedTransformers,
 });
 
+// Display a nice welcome message.
 console.log(`Welcome to the REPL example for Obscenity.
 Type ".help" for more information.`);
 
+// Start our REPL server.
 const replServer = repl.start({
 	prompt: '> ',
-	eval: (input, _ctx, _file, cb) => {
+	eval(input, _ctx, _file, cb) {
+		// Get all matches of blacklisted terms in the input. We pass 'true' to
+		// getAllMatches() so the output is sorted (easier to read).
 		const matches = matcher
 			.getAllMatches(input, true)
+			// Add some additional metadata about the phrases that were matched.
 			.map((match) => englishDataset.getPayloadWithPhraseMetadata(match));
+
+		// Return the matches to the REPL server.
 		cb(undefined, matches);
 	},
 });
 
+// Overwrite the default help command.
 replServer.defineCommand('help', {
 	help: 'View a help message',
 	action() {
