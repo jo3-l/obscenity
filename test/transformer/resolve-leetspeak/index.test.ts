a2de6fea425bd0c4901d89b4e5df95e89217d9d6
Alvaro López <rion18@hotmail.com>
2025-01-16 11:55:30 -0500

    feat: add more characters to leet transformer (#78)
    * Updated characters in leet speak transformer
    
    * Updated gitignore

--- test/transformer/resolve-leetspeak/index.test.ts
+++ test/transformer/resolve-leetspeak/index.test.ts
@@ -15,12 +15,23 @@ describe('resolveLeetSpeakTransformer()', () => {
 		it('should remap relevant characters to their normalized equivalent', () => {
 			const transformer = resolveLeetSpeakTransformer();
 			expect(transformer.transform('@'.charCodeAt(0))).toBe(CharacterCode.LowerA);
+			expect(transformer.transform('4'.charCodeAt(0))).toBe(CharacterCode.LowerA);
+			expect(transformer.transform('('.charCodeAt(0))).toBe('c'.charCodeAt(0));
+			expect(transformer.transform('3'.charCodeAt(0))).toBe('e'.charCodeAt(0));
+			expect(transformer.transform('1'.charCodeAt(0))).toBe('i'.charCodeAt(0));
+			expect(transformer.transform('!'.charCodeAt(0))).toBe('i'.charCodeAt(0));
+			expect(transformer.transform('|'.charCodeAt(0))).toBe('i'.charCodeAt(0));
+			expect(transformer.transform('6'.charCodeAt(0))).toBe('g'.charCodeAt(0));
+			expect(transformer.transform('0'.charCodeAt(0))).toBe('o'.charCodeAt(0));
 			expect(transformer.transform('$'.charCodeAt(0))).toBe('s'.charCodeAt(0));
+			expect(transformer.transform('5'.charCodeAt(0))).toBe('s'.charCodeAt(0));
+			expect(transformer.transform('7'.charCodeAt(0))).toBe('t'.charCodeAt(0));
+			expect(transformer.transform('2'.charCodeAt(0))).toBe(CharacterCode.LowerZ);
 		});
 
 		it('should leave other characters as is', () => {
 			const transformer = resolveLeetSpeakTransformer();
-			expect(transformer.transform('e'.charCodeAt(0))).toBe('e'.charCodeAt(0));
+			expect(transformer.transform('f'.charCodeAt(0))).toBe('f'.charCodeAt(0));
 			expect(transformer.transform(CharacterCode.Backslash)).toBe(CharacterCode.Backslash);
 		});
 	});
